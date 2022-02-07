fd444a0d3858491e88abc248bddbccee3764b715
Ana-ovidio <anacarolina.ovidio@estudante.ufjf.br>
2022-02-07 19:43:16 -0300

    Adicionar imagem + Validação de extensões.

--- general_system/forms.py
+++ general_system/forms.py
@@ -1,11 +1,13 @@
 # Allow the access to a form
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 # Associated with fields
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 # Restriction of same fields to users
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from general_system.models import Usuario
 from flask_login import current_user
+
 
 class FormCreateAccount(FlaskForm):
     username = StringField('Nome de usuário:', validators=[DataRequired()])
@@ -19,15 +21,18 @@ class FormCreateAccount(FlaskForm):
         if usuario:
             raise ValidationError('E-mail já cadastrado. Cadastre-se com outro e-mail ou faça login para continuar')
 
+
 class FormLogin(FlaskForm):
     email = StringField('E-mail:', validators=[DataRequired(), Email()])
     password = PasswordField('Senha:', validators=[DataRequired(), Length(6, 20)])
     remember_password = BooleanField('Aceita relembrar os dados de acesso')
     button_submit_login = SubmitField('Entrar')
 
+
 class FormEditProfile(FlaskForm):
-    username= StringField('Nome de usuário:', validators=[DataRequired()])
+    username = StringField('Nome de usuário:', validators=[DataRequired()])
     email = StringField('E-mail', validators=[DataRequired(), Email()])
+    photo_profile = FileField('Atualizar foto de perfil: ', validators=[FileAllowed(['jpg', 'png', 'jpeg'],message= 'Extensão inválida, insira com jpeg, jpg ou png.')])
     button_submit_edit_profile = SubmitField('Confirmar Edição')
 
     def validate_email(self, email):
